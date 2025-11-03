9f63fe0b284e43048d01d9c9ab19b054bda27304
ItsSitanshu <sitanshu15shrestha@gmail.com>
2025-11-03 23:47:11 +0545

    Systematicized

--- rpi/movement.py
+++ rpi/movement.py
@@ -4,30 +4,8 @@ import math
 import time
 
 from adafruit_pca9685 import PCA9685
+from maps.beta import *
 
-# todo: pull initial configs from API for easier tweaks
-COXA = 0.05 
-FEMUR = 0.08
-TIBIA = 0.12
-
-# todo: replace SERVO_MAP and NEUTRAL_ANGLES with actual values
-SERVO_MAP = {
-    0: (0, 1, 2),
-    1: (3, 4, 5),
-    2: (6, 7, 8),
-    3: (9, 10, 11),
-    4: (12, 13, 14),
-    5: (15, 16, 17)
-}
-
-NEUTRAL_ANGLES = {
-    0: (90, 90, 90),
-    1: (90, 90, 90),
-    2: (90, 90, 90),
-    3: (90, 90, 90),
-    4: (90, 90, 90),
-    5: (90, 90, 90),
-}
 
 class ServoDriver:
     def __init__(self, freq=50):
